c8b7f68eb3b0953eb49fed8ef27508c9fbfe4fe7
UnqualifiedProfessional <Matthew.head184@gmail.com>
2025-12-12 18:50:40 +0000

    h

--- babylonProj/element5/src/scene1/createStartScene.ts
+++ babylonProj/element5/src/scene1/createStartScene.ts
@@ -12,27 +12,59 @@ import {
     Engine,
     StandardMaterial,
     Texture,
-    Color3
+    Color3,
+    BoxBlock
   } from "@babylonjs/core";
   
   
-  function createCylinder(scene: Scene) {
-    let cylinder = MeshBuilder.CreateCylinder(
-      "cylinder",
-      { height: 1, diameter: 0.7 },
-      scene
-    );
-    cylinder.position.x = 1;
-    cylinder.position.y = 1;
-    cylinder.position.z = 1;
-  
-    var texture = new StandardMaterial("reflective", scene);
-    texture.ambientTexture = new Texture("./assets/textures/wood.jpg", scene);
-    texture.diffuseColor = new Color3(1, 1, 1);
-    cylinder.material = texture;
-    return cylinder;
-  }
+  function createBox(scene: Scene) {
+  const box = MeshBuilder.CreateBox(
+    "box",
+    { width: 4, height: 1 },
+    scene
+  );
+  var texture = new StandardMaterial("reflective", scene);
+  texture.ambientTexture = new Texture("./assets/textures/wood.jpg", scene);
+  texture.diffuseColor = new Color3(1, 1, 1);
 
+  box.position = new Vector3(1, 0.5, 1);
+  box.rotation.y = 1;
+  box.material = texture;
+  let box1 = box.clone("box1");
+  box1.position = new Vector3(-1, 0.5, -1);
+  box1.rotation.y = 1;
+  box1.material = texture;
+  let box2 = box.clone("box2");
+  box2.position = new Vector3(1, 1.5, -1);
+  box2.rotation.y = -1;
+  box2.material = texture;
+  let box3 = box.clone("box3");
+  box3.position = new Vector3(-1, 1.5, 1);
+  box3.rotation.y = -1;
+  box3.material = texture;
+  let box4 = box.clone("box4");
+  box4.position = new Vector3(1, 2.5, 1);
+  box4.rotation.y = 1;
+  box4.material = texture;
+  let box5 = box.clone("box5");
+  box5.position = new Vector3(-1, 2.5, -1);
+  box5.rotation.y = 1;
+  box5.material = texture;
+  let box6 = box.clone("box6");
+  box6.position = new Vector3(1, 3.5, -1);
+  box6.rotation.y = -1;
+  box6.material = texture;
+  let box7 = box.clone("box7");
+  box7.position = new Vector3(-1, 3.5, 1);
+  box7.rotation.y = -1;
+  box7.material = texture;
+  let box8 = box.clone("box7");
+  box8.position = new Vector3(-2, 1.8, -3);
+  box8.rotation.y = -0.8;
+  box8.rotation.z = 1;
+  box8.material = texture;
+  return box;
+  }
   
   function createLight(scene: Scene) {
     const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
@@ -40,24 +72,10 @@ import {
     return light;
   }
   
-  function createSphere(scene: Scene) {
-    let sphere = MeshBuilder.CreateSphere(
-      "sphere",
-      { diameter: 2, segments: 32 },
-      scene,
-    );
-    sphere.position.y = 1;
-    var texture = new StandardMaterial("reflective", scene);
-  texture.ambientTexture = new Texture("./assets/textures/wood.jpg", scene);
-  texture.diffuseColor = new Color3(1, 1, 1);
-  sphere.material = texture;
-    return sphere;
-  }
-  
   function createGround(scene: Scene) {
     let ground = MeshBuilder.CreateGround(
       "ground",
-      { width: 6, height: 6 },
+      { width: 8, height: 8 },
       scene,
     );
     var texture = new StandardMaterial("reflective", scene);
@@ -87,7 +105,7 @@ import {
   export default function createStartScene(engine: Engine) {
     interface SceneData {
       scene: Scene;
-      cylinder?: Mesh;
+      box?: Mesh;
       light?: Light;
       sphere?: Mesh;
       ground?: Mesh;
@@ -97,9 +115,8 @@ import {
     let that: SceneData = { scene: new Scene(engine) };
     // that.scene.debugLayer.show();
   
-    that.cylinder = createCylinder(that.scene);
+    that.box = createBox(that.scene);
     that.light = createLight(that.scene);
-    that.sphere = createSphere(that.scene);
     that.ground = createGround(that.scene);
     that.camera = createArcRotateCamera(that.scene);
     return that;
